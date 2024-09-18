15551fb9ecfe1e80cc730c187b147dbe6da72e89
Komal <komalraut3102@gmail.com>
2024-09-18 14:41:26 +0530

    fix: remove dist folder

--- dist/organization/interfaces/organization.interface.d.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-export interface Organization extends Document {
-    id?: string;
-    name: string;
-    adminId: string;
-    userIds: string[];
-}
--- dist/user/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    create(createUserDto: CreateUserDto): Promise<import("./interfaces/user.interface").User>;
-    findAll(): Promise<import("./interfaces/user.interface").User[]>;
-    findById(id: string): Promise<import("./interfaces/user.interface").User>;
-}
